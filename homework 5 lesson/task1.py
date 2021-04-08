0f3d478d8c14a0d6af73ec3c74c451ecac3b86e3
shulyak86 <81237417+shulyak86@users.noreply.github.com>
2021-04-08 21:23:19 +0300

    Add files via upload

--- homework 5 lesson/task1.py
+++ homework 5 lesson/task1.py
@@ -1,11 +1,20 @@
 chislo = int(input('vvedite chislo do 1000:'))
-n = 2
-while n < chislo:
-    if chislo % n != 0:
+
+
+def checker():
+    n = 2
+    if chislo in (1, 2):
         res = 'prostoe'
     else:
-        res = 'ne prostoe'
-        break
-    n += 1
-print(res)
+        while n < chislo:
+            if chislo % n != 0:
+                res = 'prostoe'
+            else:
+                res = 'ne prostoe'
+                break
+            n += 1
+    print(res)
+
+
+checker()
 
